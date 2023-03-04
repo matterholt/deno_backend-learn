05ef9d16814b3e368b907e22745c5c8c0c25c46e
matterholt <matterholt.dev@gmail.com>
2023-03-04 11:36:29 -0500

    working on routes

--- main/index.ts
+++ main/index.ts
@@ -6,39 +6,71 @@ const app = new Application();
 const router = new Router();
 
 
-interface CouresBook{
+interface CourseBook{
   id : string,
   title: string,
-  gradeLevel : string
+  gradeLevel : string,
 }
 
 
-interface CourseChapters {
+interface CourseChapter {
   id : string,
   subject : string,
   numberOfChapters : number
+  bookId : number
 }
 //MOC data
-const courses = new Map();
+const Courses = new Map<sting, CourseBook>();
+const CoursesDetals = new Map<sting, CurseChapter>();
+Courses.set(
+  '1',{
+    id: '1',
+    tilte:"beginner math",
+    gradeLevel: "2"
+  },
+)
+Courses.set(
+  '2',{
+    id : '2',
+    tilte: "beginner reading",
+    gradeLeve:"3"
+  }
+ )
+CoursesDetals.set(
+  '1',{
+    id:'10',
+    subject: 'math',
+    numberOfChapters : 20,
+    bookId: '1'
+  }
+)
 
+CoursesDetals.set(
+  '2',{
+    id:'20',
+    subject: 'reading',
+    numberOfChapters : 22,
+    bookId: '2'
+    
+  }
+)
 
 // get all courses 
 router.get('/api/courses', (ctx)=>{
-  ctx.response.body = 'Recieve a GET HTTP method'
+  ctx.response.body = Array.from(Courses.values())
 })
 
-
-// post a new course
-
-router.post('/api/courses', (ctx)=>{
-  ctx.response.body = 'Recieve a POST HTTP method'
-})
 
 // put 
-router.put('/api/courses/:courseId', (ctx)=>{
-  const { courseId} = helpers.getQuery(ctx, { mergePrams: true})
-  ctx.response.body = `Recieve a PUT HTTP method update ${ctx.params.courseId}`
+router.get('/api/courses/:courseId', (ctx)=>{
+  const { courseId} = helpers.getQuery(ctx, { mergeParams: true})
+  // ctx.response.body = `recieve a delete http method remove ${courseId}`o
+
+  // send the chapter details as well with book,
+  ctx.response.body = Courses.get(courseId)
 })
+
+
 
 // delete 
 router.delete('/api/courses/:courseId', (ctx)=>{
@@ -46,6 +78,11 @@ router.delete('/api/courses/:courseId', (ctx)=>{
   ctx.response.body = `recieve a delete http method remove ${ctx.params.courseId}`
 })
 
+// post a new course
+const ID = Deno.uid();
+router.post('/api/courses', (ctx)=>{
+  ctx.response.body = 'Recieve a POST HTTP method'
+})
 
 
 app.use(router.routes());
